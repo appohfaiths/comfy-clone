2b75a86de83537c4e835bc3c62f7123ab18a326a
Faith S. Appoh <appohfaiths@gmail.com>
2022-06-11 22:02:23 +0000

    added functions to display icons on products

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 const sidebar = document.querySelector('.sidebar');
 const cartButton = document.querySelector('.cart-button');
 const closeButton = document.querySelector('.sidebar-close-button');
+const productControls = document.querySelectorAll('.product-controls');
+const productControlsIcons = document.querySelectorAll(
+  '.product-controls-icons'
+);
 
+// Event Listeners
 cartButton.addEventListener('click', () => {
   showSidebar();
 });
@@ -10,10 +15,30 @@ closeButton.addEventListener('click', () => {
   hideSidebar();
 });
 
+for (const productControl of productControls) {
+  productControl.addEventListener('mouseover', () => {
+    showProductControlsIcons();
+  });
+  productControl.addEventListener('mouseout', () => {
+    hideProductControlsIcons();
+  });
+}
+
+// Functions
 const showSidebar = () => {
-  sidebar.classList.add('show');
+  sidebar.classList.add('show-sidebar');
 };
 
 const hideSidebar = () => {
-  sidebar.classList.remove('show');
+  sidebar.classList.remove('show-sidebar');
 };
+
+for (const productControlsIcon of productControlsIcons) {
+  function showProductControlsIcons() {
+    productControlsIcon.classList.add('show-product-controls-icons');
+  }
+
+  function hideProductControlsIcons() {
+    productControlsIcon.classList.remove('show-product-controls-icons');
+  }
+}
