4075f130a4aaac1bd50a7aad38530f2fe1b930d6
Faith S. Appoh <appohfaiths@gmail.com>
2022-06-15 09:40:36 +0000

    Added single product display page

--- js/products.js
+++ js/products.js
@@ -2,6 +2,7 @@ import { showSidebar, hideSidebar } from './index.js';
 
 const cartButton = document.querySelector('.cart-button');
 const closeButton = document.querySelector('.sidebar-close-button');
+const searchInput = document.querySelector('.search-input');
 const productNamesList = document.querySelectorAll('.product-name');
 const productNames = [...productNamesList];
 
@@ -13,4 +14,16 @@ closeButton.addEventListener('click', () => {
   hideSidebar();
 });
 
-console.log(productNamesList);
+searchInput.addEventListener('keydown', (e) => {
+  console.log(e);
+  const searchValue = e.key;
+  console.log(searchValue);
+  productNames.forEach((element) => {
+    // const firstChar = element.innerHTML.charAt(0);
+    // firstChar === searchValue ? console.log(element) : console.log('not found');
+
+    if (!element.innerHTML.includes(searchValue)) {
+      element.parentElement.style.display = 'none';
+    }
+  });
+});
